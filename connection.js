4ca4d8d151c0134086a09f53a13ff02cca8b33b5
mariohd <mariohenriquediniz@gmail.com>
2016-04-24 10:34:46 -0300

    fix connections strings

--- connection.js
+++ connection.js
@@ -1,5 +1,8 @@
 var mongoose = require ("mongoose");
 
+console.log("process.env.MONGOLAB_URI: ", process.env.MONGOLAB_URI);
+console.log("process.env.MONGOHQ_URL: ", process.env.MONGOHQ_URL);
+
 var uristring =
     process.env.MONGOLAB_URI ||
     process.env.MONGOHQ_URL || 
@@ -7,8 +10,6 @@ var uristring =
 
 mongoose.connect(uristring, function (err, res) {
   if (err) {
-    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+    console.log ('ERROR connecting to: ' + uristring + '. \n' + err);
   }
 });
-
-//mongoose.set('debug', true);

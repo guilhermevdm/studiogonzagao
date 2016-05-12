edc305cc8d74bb92819f1eaa3e928fa2f2af491c
mariohd <mariohenriquediniz@gmail.com>
2016-05-12 08:24:38 -0300

    records by genres

--- routes/statistics.js
+++ routes/statistics.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Records = require("../models/record-model");
 var Bands = require("../models/band-model");
+var Genres = require("../models/genre-model");
 var mongoose = require ("mongoose");
 
 router.get('/', function(req, res, next) {
@@ -32,6 +33,38 @@ router.get('/bands', function (req, res) {
 					name: band.name,
 					count: result.filter(function (r) {
 						return r._id == band.id;
+					})[0].count
+				});
+
+			});
+			res.json(response);
+		});
+	});
+});
+
+router.get('/genres', function (req, res) {
+	Records.aggregate([
+		{ $match: { user: mongoose.Types.ObjectId(req.session.user._id) } },
+		{ $group: { 
+				_id: '$genre',
+				count: {$sum: 1}
+			} 
+		},
+	], function (err, result) {
+		var genres = result.map(function (genre) {
+			return genre._id;
+		});
+		Genres.find({
+			_id: { $in: genres }
+		}, function (e, genres) {
+			if (e) return res.status(500).json(e);
+			var response = [];
+			genres.forEach(function (genre) {
+
+				response.push({
+					name: genre.name,
+					count: result.filter(function (r) {
+						return r._id == genre.id;
 					})[0].count
 				});
 
